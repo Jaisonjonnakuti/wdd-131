de55425a8c7fab0fc124ee08590a59257819181a
jaison jonnakuti <jon24039@byui.edu>
2025-10-04 19:33:27 -0600

    Add files via upload

---
+++ cool.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector(".menu");
+  const nav = document.querySelector("nav");
+
+  menuButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+});
